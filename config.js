a97fa79a627b19e3040cec0758f1e5d41df0ebda
bybrkaydn <gizliicerikvideo@gmail.com>
2021-09-19 00:47:32 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = {
+
+    plotfolder: './plots/',
+    gfolder: '1tNU82Hx8HRNqw1rH6qErATQ8jLh0mGuO',
+
+    //################### ACCOUNT LIST ########################
+
+    token1: '{"access_token":"ya29.a0ARrdaM8A9WWG1z854STkiOeXHGoaj4WSiuv6LyYva7OTtiu7mFienBWn7F_yA3DAqTVrym1L-dMMwBZgjin6AxUp5EIhep17GNOIB_waMiEvyCeV4V31d_08JosgheJ_HHIbpTcUP9X1vNqwTMQOYlnyak7J","refresh_token":"1//09w0AzhS9QDGYCgYIARAAGAkSNwF-L9IrjP_TI_4ctPRqWNbAW0sBH9mLQeEA322QEWqND5yUv7TUddbDuqfedUw_J-oGPBuj71k","scope":"https://www.googleapis.com/auth/drive","token_type":"Bearer","expiry_date":1631172563639}',
+    credentials1:'{"installed":{"client_id":"64803094643-pjnmbhidspeq35aonn9vctpkgrp9n1he.apps.googleusercontent.com","project_id":"durable-trainer-325506","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"pkMveGqW8OHnY-Ot7gpUE5Yn","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}',
+
+    token2: '{"access_token":"ya29.a0ARrdaM_yl3o_Iwaymm03-qTSzrjIKT7sBsPewb34BnJuknbbG9_mdtKaLku1v-e_8HXoCunQkE1G83vm9q96g-cO8WQS7JMtvjHeqPrj1jQnKALhZ4SreZlx8IigB7fbzCdIL7viUeFir1OdzqTz5a5LZC2N","refresh_token":"1//09IGIc79kNN8MCgYIARAAGAkSNwF-L9Irt9gmSZordpycrIFNeuPc0Lh6AEXco5MDrQHwR1r2v3kLP2PK9DEA_mdsxigNKD_lhgw","scope":"https://www.googleapis.com/auth/drive","token_type":"Bearer","expiry_date":1631206820930}',
+    credentials2: '{"installed":{"client_id":"456605800853-4jtol5gq8ccokjf3a96v27no846235g6.apps.googleusercontent.com","project_id":"diesel-well-325515","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"VK4YW2GAY9x8G6dPkQzwZKky","redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"]}}',
+
+
+};
+
+Object.freeze(module.exports);
